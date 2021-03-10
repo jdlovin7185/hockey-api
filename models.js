39d8075112cfba2e2db8330093b158ddaddc5514
Josh Lovin <76003574+jdlovin7185@users.noreply.github.com>
2021-03-10 17:13:12 -0500

    Adds models

---
+++ models.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+let playerSchema = mongoose.Schema({
+    Name: {type: String, required true},
+    Position: {
+      Type: String,
+      Description: String
+    },
+    Stats: {
+      GamesPlayed: String,
+      Goals: String,
+      Assists: String,
+      Points: String,
+      PenaltyMinutes: String
+    },
+    Bio: {
+      Birth: Date,
+      NHLTeam: String,
+      Weight: String,
+      Height: String, 
+      Description: String
+    },
+    ImagePath: String
+});
+
+let userSchema = mongoose.Schema({
+  Username: {type: String, required: true},
+  Password: {type: String, required: true},
+  Email: {type: String, required: true},
+  FavoritePlayer: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Player'}]
+});
+
+let Player = mongoose.model('Player', playerSchema);
+let User = mongoose.model('User', userSchema);
+
+module.exports.Player = Player;
+module.exports.User = User;
